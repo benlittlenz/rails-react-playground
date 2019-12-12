ebc90a3dd87d3823a45421f501533938336646e6
nzbenji <benlittle53@gmail.com>
2019-12-12 18:59:27 +1300

    switched to useMutation hook

--- frontend/src/Users.js
+++ frontend/src/Users.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import UserAvatar from "./UserAvatar";
-
+import CreateUser from "./CreateUser";
 
 const GET_USERS = gql`
   {
@@ -15,8 +15,28 @@ const GET_USERS = gql`
   }
 `;
 
-function Users({selectUser}) {
+function Users({ selectUser }) {
   const { loading, error, data } = useQuery(GET_USERS);
+
+  function updateUsers(cache, { data: { createUser }}) {
+    const { users } = cache.readQuery({ query: GET_USERS });
+    cache.writeQuery({
+      query: GET_USERS,
+      data: { users: users.concat([createUser.user]) },
+    });
+  }
+
+  // const [addUser] = useMutation(
+  //   ADD_TODO,
+  //   {
+  //     update(cache, { data: { addUser } }) {
+  //       const { todos } = cache.readQuery({ query: GET_USERS });
+  //       cache.writeQuery({
+  //         query: GET_USERS,
+  //         data: { todos: todos.concat([addUser]) },
+  //       });
+  //     }
+  //   })
 
   if (loading) return "Loading...";
   if (error) return `Error: ${error.message}`;
@@ -26,11 +46,13 @@ function Users({selectUser}) {
       {data.users.map(user => (
         <div
           className="lg:w-1/3 w-full p-4 text-center inline"
-          onClick={selectUser.bind(this,user)}
+          onClick={selectUser.bind(this, user)}
         >
           <UserAvatar user={user} />
         </div>
       ))}
+
+      <CreateUser onCreateUser={updateUsers} />
     </div>
   );
 }
